d3b48e51bcbcaa4c5534394b9c14408f19311e10
Moses jafar Hanon <94764725+Moseseng@users.noreply.github.com>
2022-08-11 03:22:21 +0300

    Update ML.py

--- logic-task-/ML.py
+++ logic-task-/ML.py
@@ -1,11 +1,10 @@
-
-
+#q1
 string = "Mousa"
 string1 = string.replace("u", "") 
 
 print(string1)
 
-
+#q2
 n = 100
 s=[]
 
@@ -13,5 +12,12 @@ for i in range(2,n):
     if i % 2 == 0:
         s.append(i)
 print(s)
+
+a = "MMIIJK"
+
+#q3
+for i in a:
+    f=a.count(i)
+    print(i,"=",f)
 
  
